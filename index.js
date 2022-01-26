b105ab9a6b149b8ee8d6784e8f1960160d0f9af8
heraldofortuna <heraldofortuna@gmail.com>
2022-01-26 14:18:19 -0500

    Los estilos de la aplicación fueron añadidos además de añadir la funcionalidad de los mensajes de error en App.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+app.use(express.static("build"));
 
 // Tomamos el middleware para usar y permitir solicitudes
 // de todos los orígenes
